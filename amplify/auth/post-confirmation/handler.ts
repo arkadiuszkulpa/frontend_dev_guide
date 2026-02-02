eb460b191bba4a0e76214d1b14fca15e7ee6468b
arkadiuszkulpa <arkadiusz.kulpa.91@gmail.com>
2026-02-02 13:35:46 +0000

    fix for the amplify process.env import to facilitate groups

--- amplify/auth/post-confirmation/handler.ts
+++ amplify/auth/post-confirmation/handler.ts
@@ -3,13 +3,15 @@ import {
   CognitoIdentityProviderClient,
   AdminAddUserToGroupCommand,
 } from '@aws-sdk/client-cognito-identity-provider';
-import { env } from '$amplify/env/post-confirmation';
 
 const client = new CognitoIdentityProviderClient();
 
+// Group name is set via environment variable in resource.ts
+const GROUP_NAME = process.env.GROUP_NAME || 'Users';
+
 export const handler: PostConfirmationTriggerHandler = async (event) => {
   const command = new AdminAddUserToGroupCommand({
-    GroupName: env.GROUP_NAME,
+    GroupName: GROUP_NAME,
     Username: event.userName,
     UserPoolId: event.userPoolId,
   });
